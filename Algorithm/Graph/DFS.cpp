eefb646ab1cd1863bd46e71c62df4eb4f0524d2e
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2021-05-07 01:19:38 +0530

    formatting fix

--- Algorithm/Graph/DFS.cpp
+++ Algorithm/Graph/DFS.cpp
@@ -1,12 +1,6 @@
 void DFS(int Node) {
 	visited[Node] = true;
-
-
-
-
 	for(auto Neighbour: adj[Node]){
-		if(not visited[Neighbour])
-
-			DFS(Neighbour);
+		if(not visited[Neighbour]) DFS(Neighbour);
 	}
 }
