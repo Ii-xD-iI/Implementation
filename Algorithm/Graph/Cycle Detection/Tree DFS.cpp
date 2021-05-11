3a6e530d904220333afba9c56bab3e7a08886a7d
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2021-05-11 21:15:46 +0530

    1 based indexing

---
+++ Algorithm/Graph/Cycle Detection/Tree DFS.cpp
@@ -0,0 +1,52 @@
+using namespace std;
+#include "bits/stdc++.h"
+
+bool CycleDetection(int Node, int Parent);
+
+bool Cycle = false;
+int Nodes,Edges;
+vector<vector<int>> Adj(Nodes+7);//Adj_List
+vector<int> Visited(Nodes +7);
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(0);
+
+    cin>>Nodes>>Edges;
+    for(int i {};i<Edges; ++i) {
+        int U,V;
+        cin>>U>>V;
+        // U --> V 
+        Adj[U].emplace_back(V);
+        // Adj[V].emplace_back(U);
+    }
+
+    
+    for(int i=1;i<=Nodes;++i) {
+        if( (not Visited[i]) and CycleDetection(i, -1) ) {//curr Node, Parent
+            Cycle= true;
+        }
+    }
+
+    cout << (Cycle? "Cycle Detected\n": "No Cycle was found\n");
+    return 0;
+
+}
+
+bool CycleDetection(int Node, int Parent) {
+    Visited[Node] = true;
+
+    for(auto &Child: Adj[Node]) {
+        if(Child == Parent) continue;
+
+        if(Visited[Child]) 
+            return true;
+        
+        else if( ( not Visited[Child]) and CycleDetection(Child, Node)) {
+            return true;
+        }
+    }
+
+    return false;
+}
