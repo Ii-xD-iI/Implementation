fcbfbe47bbe1cdbb96cbfacd74f574b3641785b9
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2021-05-16 22:54:36 +0530

    bug fixed

--- DataStructure/UFDS.cpp
+++ DataStructure/UFDS.cpp
@@ -1,46 +1,40 @@
-class UFDS {
-
-    int n;//number of nodes
-    int cnt{};//number of Connected Comps 
-    vector <int> par;//parent of node i is par[i]
-    vector<int> sz;//size of the component where i belongs == 1 by default
-    vector<int> rank;
+class UFDS { //Clueless OTZ
     public:
-        /*Clueless OTZ*/
-        UFDS(int _) : n(_) {
-            cnt=n;
-            par.resize(n+7),sz.resize(n+7),rank.resize(n+1);
-            fill(sz.begin(), sz.end(),0);
+
+	    int n,cnt{};//number of [nodes, Connected Comps]
+    	vector <int> par;//par[i] = parent of node i
+    	vector<int> sz;
+
+        UFDS(int _) : n(_) {            
+            cnt=n;//number of cc
+            par.resize(n+7), sz.resize(n+7);
+            fill(sz.begin(), sz.end(),1);
             iota(par.begin(), par.end(),0);
         }
     
-        int Find(int u) {
-            return (par[u] == u? u : par[u] = Find(par[u]));//+path compression
-        }
+        int Find(int u) 
+        	return (par[u] == u? u : par[u] = Find(par[u]));
     
-        bool Connected(int u, int v) {
-            return Find(u) == Find(v);//if connected 1,else 0
-        }
-        
-        int Len(int u){
-            return sz[Find(u)];
-        }
+        bool Connected(int u, int v) 
+        	return ( Find(u) == Find(v) );
 
-        bool Union(int u, int v) {//merge the set of those 2 nodes
-            u = Find(u),v = Find(v);//find root of (_)
+        int Len(int u) 
+        	return sz[Find(u)];
+    
+        bool Union(int u, int v) {
+            u = Find(u),v = Find(v);
             
-            if(u==v){return false;}//iz not guranteed that its disjoint
+            if(u==v) 
+                return false;
             
-            if (rank[u] < rank[v]) swap(u, v);
-
+            if (sz[u] < sz[v]) swap(u, v);
+            
             par[v] = u;
-
-            if(rank[u] == rank[v]) ++rank[u];
-            
             sz[u] += sz[v]; 
-            --cnt;//2 comps got merged 
-            return 1;
+            --cnt; 
+            
+            return true;
         }
-        //Use Len() instead of finding it from the sz vector manually,
-        //because only the size of the root of each set is updated.
+        /*Use Len() instead of finding it from the sz vector manually,
+          because only the size of the root of each set is updated.*/
 };
