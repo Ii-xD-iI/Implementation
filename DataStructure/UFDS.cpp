fcc0fc254b0ac772813a73eac5afef344f4601a8
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2021-05-17 02:31:05 +0530

    Final :sanapray:

--- DataStructure/UFDS.cpp
+++ DataStructure/UFDS.cpp
@@ -8,18 +8,22 @@ class UFDS { //Clueless OTZ
         UFDS(int _) : n(_) {            
             cnt=n;//number of cc
             par.resize(n+7), sz.resize(n+7);
+
             fill(sz.begin(), sz.end(),1);
             iota(par.begin(), par.end(),0);
         }
     
-        int Find(int u) 
+        int Find(int u) {
         	return (par[u] == u? u : par[u] = Find(par[u]));
+        }
     
-        bool Connected(int u, int v) 
-        	return ( Find(u) == Find(v) );
+        bool Connected(int u, int v) {
+        	return ( Find(u) == Find(v) );        	
+        } 
 
-        int Len(int u) 
+        int Len(int u) {
         	return sz[Find(u)];
+        }
     
         bool Union(int u, int v) {
             u = Find(u),v = Find(v);
@@ -38,3 +42,6 @@ class UFDS { //Clueless OTZ
         /*Use Len() instead of finding it from the sz vector manually,
           because only the size of the root of each set is updated.*/
 };
+/*
+5/17/2021 2:30 am [m/dd/yyyy]
+*/
