2012a8f636683c0d6718504d592b566b769a9f93
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2021-08-15 20:27:07 +0530

    UPD : tab sz 4 -> 2

--- DataStructure/UFDS.cpp
+++ DataStructure/UFDS.cpp
@@ -1,47 +1,44 @@
 class UFDS { //Clueless OTZ
-    public:
+  public:
 
-	    int n,cnt{};//number of [nodes, Connected Comps]
-    	vector <int> par;//par[i] = parent of node i
-    	vector<int> sz;
+    int n,cnt{};//number of [nodes, Connected Comps]
+    vector <int> par;//par[i] = parent of node i
+    vector<int> sz;
 
-        UFDS(int _) : n(_) {            
-            cnt=n;//number of cc
-            par.resize(n+7), sz.resize(n+7);
+    UFDS(int _) : n(_) {            
+      cnt=n;//number of cc
+      par.resize(n+7), sz.resize(n+7);
 
-            fill(sz.begin(), sz.end(),1);
-            iota(par.begin(), par.end(),0);
-        }
-    
-        int Find(int u) {
-        	return (par[u] == u? u : par[u] = Find(par[u]));
-        }
-    
-        bool Connected(int u, int v) {
-        	return ( Find(u) == Find(v) );        	
-        } 
+      fill(sz.begin(), sz.end(),1);
+      iota(par.begin(), par.end(),0);
+    }
 
-        int Len(int u) {
-        	return sz[Find(u)];
-        }
-    
-        bool Union(int u, int v) {
-            u = Find(u),v = Find(v);
-            
-            if(u==v) 
-                return false;
-            
-            if (sz[u] < sz[v]) swap(u, v);
-            
-            par[v] = u;
-            sz[u] += sz[v]; 
-            --cnt; 
-            
-            return true;
-        }
-        /*Use Len() instead of finding it from the sz vector manually,
-          because only the size of the root of each set is updated.*/
+    int Find(int u) {
+      return (par[u] == u? u : par[u] = Find(par[u]));
+    }
+
+    bool Connected(int u, int v) {
+      return ( Find(u) == Find(v) );          
+    } 
+
+    int Len(int u) {
+      return sz[Find(u)];
+    }
+
+    bool Union(int u, int v) {
+      u = Find(u),v = Find(v);
+      
+      if(u==v) 
+        return false;
+      
+      if (sz[u] < sz[v]) swap(u, v);
+      
+      par[v] = u;
+      sz[u] += sz[v]; 
+      --cnt; 
+      
+      return true;
+  }
+  /*Use Len() instead of finding it from the sz vector manually,
+    because only the size of the root of each set is updated.*/
 };
-/*
-5/17/2021 2:30 am [m/dd/yyyy]
-*/
